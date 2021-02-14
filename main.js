92bb4f8dd862218b3c0f1cc6ee44b42fa2c03e62
tvani79 <75181652+tvani79@users.noreply.github.com>
2021-02-13 23:24:28 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var console_project="My first console project"
+var first_name="Swara";
+var last_name="Igoor";
+var full_name=first_name.concat(last_name);
+console.log("The full name is "+full_name);
+var length_of_full_name=full_name.length;
+console.log("The length of the name is "+length_of_full_name);
+var addition=Math.addition(257+398);
+console.log("The sum of these two numbers is "+addition);
